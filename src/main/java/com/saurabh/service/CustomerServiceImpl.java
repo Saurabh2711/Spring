3fe0ff6820984e5bf01f32e9bb699efacbc6aa44
Saurabh Uttam <saurabh.uttam@inmobi.com>
2017-08-27 16:04:42 +0530

    Added annotation

--- src/main/java/com/saurabh/service/CustomerServiceImpl.java
+++ src/main/java/com/saurabh/service/CustomerServiceImpl.java
@@ -2,15 +2,20 @@ package com.saurabh.service;
 
 import com.saurabh.model.Customer;
 import com.saurabh.repository.CustomerRepository;
-import com.saurabh.repository.HibernateCustomerRepositoryImpl;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * Created by saurabh.uttam on 8/26/17.
  */
+
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+
+    @Resource(name = "customerRepository")
+    private CustomerRepository customerRepository;  //Moved to spring  = new HibernateCustomerRepositoryImpl();
 
     @Override
     public List<Customer> findAll() {
