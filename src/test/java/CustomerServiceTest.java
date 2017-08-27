407f71dfb2db420fd9f89ba735ceb8c773475aaf
Saurabh Uttam <saurabh.uttam@inmobi.com>
2017-08-27 16:27:59 +0530

    Added setter injection using java config

--- src/test/java/CustomerServiceTest.java
+++ src/test/java/CustomerServiceTest.java
@@ -1,12 +1,16 @@
 import com.saurabh.service.CustomerService;
 import com.saurabh.service.CustomerServiceImpl;
+import config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Created by saurabh.uttam on 8/27/17.
  */
 public class CustomerServiceTest {
     public static void main(String[] args) {
-        CustomerService customerService= new CustomerServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        CustomerService customerService = applicationContext.getBean("customerService",CustomerService.class);
         System.out.println(customerService.findAll().get(0).getFirstname());
         System.out.println(customerService.findAll().get(0).getLastname());
     }
